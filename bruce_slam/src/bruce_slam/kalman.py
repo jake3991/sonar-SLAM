26ec37ad1314ea2257efca2d928a679ce44afc7e
chloepth <105660290+chloepth@users.noreply.github.com>
2022-06-09 21:35:58 +0200

    Add files via upload

--- bruce_slam/src/bruce_slam/kalman.py
+++ bruce_slam/src/bruce_slam/kalman.py
@@ -4,9 +4,7 @@ import tf
 import rospy
 import gtsam
 import numpy as np
-
 import rospy
-from std_msgs.msg import String, Float32
 
 # ros-python imports
 from nav_msgs.msg import Odometry
@@ -23,103 +21,146 @@ from bruce_slam.utils.topics import *
 from bruce_slam.utils.conversions import *
 from bruce_slam.utils.io import *
 from bruce_slam.utils.visualization import ros_colorline_trajectory
-#---
+
+# new imports
+from std_msgs.msg import String, Float32
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 import math
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
+
 class KalmanNode(object):
+	'''A class to support Kalman using DVL and IMU readings
+	'''
 
 	def __init__(self):
 		#state vector = (x,y,z,roll, pitch, yaw, x_dot,y_dot,z_dot,roll_dot,pitch_dot,yaw_dot)
-		# nice comment here, this makes it very easy to follow what you are doing
 		self.state_vector= np.array([[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
-		self.cov_matrix= np.diag([0,0,0,0,0,0,0,0,0,0,0,0])
+		self.cov_matrix= np.diag([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+		self.Q = np.eye(12,12) #proccess noise
+		self.R = np.eye(3,3)
+		self.dt_dvl = 0.2 # 5Hz
+		self.H_dvl = np.array([
+		[0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0.,],
+		[0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0.,],
+		[0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.,]])
+		self.dt_imu = 0.005 # 200Hz
+		self.H_imu = np.array([
+		[0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
+		[0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
+		[0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0.]])
 
 
 	def init_node(self, ns="~")->None:
+		"""Init the node, fetch all paramaters from ROS
+
+		Args:
+			ns (str, optional): The namespace of the node. Defaults to "~".
+		"""
 		self.dvl_sub = rospy.Subscriber(DVL_TOPIC,DVL,callback=self.dvl_callback,queue_size=10)
-		self.pub = rospy.Publisher("state_vector_with_kalman",PoseStamped,queue_size=10) # change queue size to 250
+		self.pub = rospy.Publisher("state_vector_with_kalman",PoseStamped,queue_size=250)
 		self.pubtheta = rospy.Publisher("yaw_kalman_topic",Float32,queue_size=250)
 
 		if rospy.get_param(ns + "imu_version") == 1:
-			self.imu_sub = rospy.Subscriber(IMU_TOPIC, Imu,callback=self.imu_callback,queue_size=10) # change queue size to 250
+			self.imu_sub = rospy.Subscriber(IMU_TOPIC, Imu,callback=self.imu_callback,queue_size=250)
 		elif rospy.get_param(ns + "imu_version") == 2:
-			self.imu_sub = rospy.Subscriber(IMU_TOPIC_MK_II, Imu, callback=self.imu_callback,queue_size=10) # change queue size to 250
-		# self.tf = tf.TransformBroadcaster()
-		loginfo("Localization node is initialized") # change this to "kalman node is initialized"
+			self.imu_sub = rospy.Subscriber(IMU_TOPIC_MK_II, Imu, callback=self.imu_callback,queue_size=250)
 
-	# not clear to me what the "up" in xup is supposed to mean
-	# let's add type hints and docstring to all functions, I did this one so you have an idea
-	def kalman_predict(self,xup:np.array,Pup:np.array,dt:float):
-		"""Write a BREIF summary of what this function does
+		loginfo("Kalman Node is initialized")
+
+
+	def kalman_predict(self,previous_x:np.array,previous_P:np.array,dt:float):
+		"""Project the state and the error covariance ahead.
 
 		Args:
-			xup (np.array): _description_
-			Pup (np.array): _description_
-			dt (float): _description_
+			previous_x (np.array): value of the previous state vector
+			previous_P (np.array): value of the previous covariance matrix
+			dt (float): may be dt_imu or dt_dvl
+
+		Returns:
+			predicted_x (np.array): predicted estimation
+			predicted_P (np.array): predicted covariance matrix
 		"""
-		# is this the A matrix we defined together I personally find this a bit confusing, mainly because of the syntax
-		# can we make this a class varible and write it out super neat like you did the state vector?
-		A = np.eye(12,12) + np.diag(np.ones(6)*dt,6) 
-		P1 = A @ Pup @ A.T # this equation is not complete, we need to do P = A*P*A_transpose + Q. Q is proccess noise, defined by you I reccomend a diag matrix
-		x1 = A @ xup 
-		self.state_vector,self.cov_matrix = x1,P1
+		A = np.array([
+		[1. , 0. , 0. , 0. , 0. , 0. , dt, 0. , 0. , 0. , 0. , 0. ],
+		[0. , 1. , 0. , 0. , 0. , 0. , 0. , dt, 0. , 0. , 0. , 0. ],
+		[0. , 0. , 1. , 0. , 0. , 0. , 0. , 0. , dt, 0. , 0. , 0. ],
+		[0. , 0. , 0. , 1. , 0. , 0. , 0. , 0. , 0. , dt, 0. , 0. ],
+		[0. , 0. , 0. , 0. , 1. , 0. , 0. , 0. , 0. , 0. , dt, 0. ],
+		[0. , 0. , 0. , 0. , 0. , 1. , 0. , 0. , 0. , 0. , 0. , dt],
+		[0. , 0. , 0. , 0. , 0. , 0. , 1. , 0. , 0. , 0. , 0. , 0. ],
+		[0. , 0. , 0. , 0. , 0. , 0. , 0. , 1. , 0. , 0. , 0. , 0. ],
+		[0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 1. , 0. , 0. , 0. ],
+		[0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 1. , 0. , 0. ],
+		[0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 1. , 0. ],
+		[0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 1. ]])
 
-	# add type hints and doc string
-	# fix spelling on kalman correc->kalman_correct
-	def kalman_correc(self,x0,P,z,H):
-		R = np.eye(3,3) # makes sense that this is diag matrix, but we want to be able to tune the values in this matrix. Let's make it a class varible that is easy to change the contents
-		K = P @ H.T @ inv(H@P@H.T + R)
-		xup = x0 + K @ (z-H@x0)
-		Pup = (np.eye(len(x0))-K @ H) @ P # this equation is incomplete
-		return(xup,Pup)
+		predicted_P = A @ previous_P @ A.T + self.Q
+		predicted_x = A @ previous_x
+		return predicted_x, predicted_P
 
-	# add doc string
+
+	def kalman_correct(self,predicted_x:np.array,predicted_P:np.array,z:np.array,H:np.array):
+		"""Measurement Update.
+
+		Args:
+			predicted_x (np.array): predicted state vector with kalman_predict()
+			predicted_P (np.array): predicted covariance matrix with kalman_predict()
+			z (np.array): measurement
+			H (np.array): may be H_dvl or H_imu
+
+		Returns:
+			corrected_x (np.array): corrected estimation
+			corrected_P (np.array): corrected covariance matrix
+
+		"""
+		K = predicted_P @ H.T @ inv(H @ predicted_P @ H.T + self.R)
+		corrected_x = predicted_x + K @ (z - H @ predicted_x)
+		corrected_P = predicted_P - K @ H @ predicted_P
+		return corrected_x, corrected_P
+
+
 	def dvl_callback(self, dvl_msg:DVL)->None:
-		dt=0.2 #5Hz # move to class varible self.dvl_dt
-		H = np.zeros((3,12)) # move to class varible, self.H_dvl
-		H[0,6]=H[1,7]=H[2,8]=1
+		"""Handle the Kalman Filter using the DVL only. Publish the state vector.
+
+		Args:
+			dvl_msg (DVL): the message from the DVL
+		"""
 		vel = np.array([[dvl_msg.velocity.x], [dvl_msg.velocity.y], [dvl_msg.velocity.z]])
 
-		self.state_vector[6,0]=dvl_msg.velocity.x # we are updating the state vector below using the kalman filter
-		self.state_vector[7,0]=dvl_msg.velocity.y # no need for this, but you can comment your correction function
-		self.state_vector[8,0]=dvl_msg.velocity.z # and use this code to test your prediction function
+		predicted_x, predicted_P = self.kalman_predict(self.state_vector, self.cov_matrix, self.dt_dvl)
+		corrected_x,corrected_P = self.kalman_correct(predicted_x, predicted_P, vel, self.H_dvl)
+		self.state_vector, self.cov_matrix = corrected_x, corrected_P
 
-		# we want to do predict then correct
-		xup,Pup = self.kalman_correc(self.state_vector,self.cov_matrix,vel,H) # once we need complete the kalman correct function we need to set our state to it
-		# so here we would say
-		# self.state_vector = xup
-		# self.covariance_matrix = Pup
-		self.kalman_predict(xup,Pup,dt)
 		self.send_state_vector(self.state_vector,dvl_msg.header.stamp)
 
-	# add docstring
+
 	def imu_callback(self, imu_msg:Imu)->None:
-		dt=0.005 #200Hz # move to class varible
-		H = np.zeros((3,12)) # move to clas varible H_imu
-		H[0,3]=H[1,4]=H[2,5]=1
+		"""Handle the Kalman Filter using the VN100 only.
+
+		Args:
+			imu_msg (Imu): the message from VN100
+		"""
 		quaternion = (imu_msg.orientation.x,imu_msg.orientation.y,imu_msg.orientation.z,imu_msg.orientation.w)
 		roll_x, pitch_y, yaw_z = euler_from_quaternion(quaternion)
 		euler_angle = np.array([[roll_x], [pitch_y], [yaw_z]])
 
-		self.state_vector[3,0] = roll_x # see note in dvl_callback, we don't need to do this
-		self.state_vector[4,0] = pitch_y
-		self.state_vector[5,0] = yaw_z
+		predicted_x, predicted_P = self.kalman_predict(self.state_vector, self.cov_matrix, self.dt_imu)
+		corrected_x,corrected_P = self.kalman_correct(predicted_x, predicted_P, euler_angle, self.H_imu)
+		self.state_vector, self.cov_matrix = corrected_x, corrected_P
 
-		# we want to do predict then correct
-		xup,Pup = self.kalman_correc(self.state_vector,self.cov_matrix,euler_angle,H) 
-		# self.state_vector = xup
-		# self.covariance_matrix = Pup
-		self.kalman_predict(xup,Pup,dt)
-		# self.send_state_vector(self.state_vector) -> already send in dvl_callback
 
-	# type hints and docstring
-	def send_state_vector(self,state_vector,t):
+	def send_state_vector(self,state_vector:np.array,t:float):
+		"""Publish the state vector : pose (x,y,z) and orientation (x,y,z,w).
+
+		Args:
+			state_vector (np.array): value of the state vector
+			dt (float): dvl_msg.header.stamp
+		"""
 		msg = PoseStamped()
-		msg.header.stamp = t #not sure
+		msg.header.stamp = t # took dvl_msg.header.stamp
 		msg.header.frame_id = "map"
 		msg.pose.position.x = state_vector[0,0]
 		msg.pose.position.y = state_vector[1,0]
@@ -135,8 +176,13 @@ class KalmanNode(object):
 
 		self.pub.publish(msg)
 
-	# type hints and docstring
-	def send_theta(self,yaw):
+
+	def send_theta(self,yaw:float):
+		"""Publish the yaw.
+
+		Args:
+			yaw (float): theta
+		"""
 		msg = Float32()
 		msg.data = yaw
 		self.pubtheta.publish(msg)
