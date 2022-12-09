d40d7af79e8a86ff545a204fa767ddad149ecaa6
jake3991 <you@example.com>
2022-12-09 10:35:22 -0500

    Clean up on heat map

--- bruce_slam/notebooks/heat_map.py
+++ bruce_slam/notebooks/heat_map.py
@@ -57,9 +57,9 @@ d = pd.DataFrame(heat_map)
 # Set up the matplotlib figure
 f, ax = plt.subplots(figsize=(20,10))
 
-# Generate a custom diverging colormap
-#cmap = sns.diverging_palette(np.max(heat_map), np.min(heat_map))
-cmap = sns.color_palette("magma", as_cmap=True, n_colors=20)
+# generate a heatmap
+#cmap = sns.color_palette("magma", as_cmap=True, n_colors=20)
+cmap = sns.color_palette("plasma", as_cmap=True, n_colors=20)
 
 # Draw the heatmap with the mask and correct aspect ratio
 sns.heatmap(d, cmap=cmap,square=True, annot = labels, linewidths=.5, cbar_kws={"shrink": 0.5})
@@ -69,12 +69,17 @@ plt.xticks([0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5],
            ["1","2","3","4","5","1","2","3","4","5","1","2","3","4","5"],fontsize=20)
 plt.yticks([0.5,1.5,2.5],["30","60","90"],fontsize=20)
 
+# add the method labels
 secax = ax.secondary_xaxis('top')
 secax.set_xticks([2.5,7.5,12.5])
 secax.set_xticklabels(["Fusion","Inference","Submap"],fontsize=30)
+
+# plot the dividing lines between the methods
 for i in range(5,15,5):
     plt.plot([i,i],[0,3],linewidth=8,linestyle="-",color="white")
     
+# add the axis labels and log
 plt.xlabel("Keyframe Translation",fontsize = 20)
 plt.ylabel("Keyframe Rotation",fontsize = 20)
+plt.savefig("reports/"+scene+"/heatmap.png")
 plt.show()
