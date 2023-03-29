45eef45ceef30dfc986fc36c33f3d0cb369bc19e
jake3991 <you@example.com>
2023-03-29 15:51:39 -0400

    Updating for holoocean use

--- bruce_slam/src/bruce_slam/feature_extraction.py
+++ bruce_slam/src/bruce_slam/feature_extraction.py
@@ -15,6 +15,7 @@ from bruce_slam import pcl
 import matplotlib.pyplot as plt
 from sonar_oculus.msg import OculusPing, OculusPingUncompressed
 from scipy.interpolate import interp1d
+import holoocean
 
 from .utils import *
 from .sonar import *
@@ -78,7 +79,7 @@ class FeatureExtraction(object):
     def configure(self):
         '''Calls the CFAR class constructor for the featureExtraction class
         '''
-        self.detector = CFAR(self.Ntc, self.Ngc, self.Pfa, self.rank)
+        self.detector = CFAR(self.Ntc, self.Ngc, self.Pfa, None)
 
     def init_node(self, ns="~"):
 
@@ -128,49 +129,58 @@ class FeatureExtraction(object):
         #vis publish topic
         self.feature_img_pub = rospy.Publisher(
             SONAR_FEATURE_IMG_TOPIC, Image, queue_size=10)
+        
+        self.sonar_config = holoocean.packagemanager.get_scenario("test")
 
         self.configure()
 
-    def generate_map_xy(self, ping):
-        '''Generate a mesh grid map for the sonar image, this enables converison to cartisian from the 
-        source polar images
+    def generate_map(self,config: dict):
+        """Generate a map to convert a holoocean sonar image from 
+        polar to cartisian coords. Note this does not apply the remapping
+        it generates the tools to apply cv2.remap later. 
 
-        ping: OculusPing message
-        '''
+        Args:
+            config (dict): the holoocean config file. Read in using
+            holoocean.packagemanager.get_scenario(scenario)
 
-        #get the parameters from the ping message
-        _res = ping.range_resolution
-        _height = ping.num_ranges * _res
-        _rows = ping.num_ranges
-        _width = np.sin(
-            self.to_rad(ping.bearings[-1] - ping.bearings[0]) / 2) * _height * 2
-        _cols = int(np.ceil(_width / _res))
+        """
 
-        #check if the parameters have changed
-        if self.res == _res and self.height == _height and self.rows == _rows and self.width == _width and self.cols == _cols:
-            return
+        # parse out the parameters
+        config = config['agents'][0]['sensors'][-1]["configuration"]
+        horizontal_fov = float(config['Azimuth'])
+        range_resolution = (config['RangeMax'] - config['RangeMin']) / 512
+        height = range_resolution * config['RangeBins']
+        rows = config['RangeBins']
+        width = np.sin(np.radians(horizontal_fov / 2)) * height * 2
+        cols = int(np.ceil(width / range_resolution))
+        bearings = np.radians(np.linspace(-horizontal_fov/2, horizontal_fov/2, config['AzimuthBins']))
+        
+        self.height = height
+        self.rows = rows
+        self.width = width
+        self.cols = cols
 
-        #if they have changed do some work    
-        self.res, self.height, self.rows, self.width, self.cols = _res, _height, _rows, _width, _cols
-
-        #generate the mapping
-        bearings = self.to_rad(np.asarray(ping.bearings, dtype=np.float32))
+        # create an interpolation object for bearing angle
         f_bearings = interp1d(
-            bearings,
-            range(len(bearings)),
-            kind='linear',
-            bounds_error=False,
-            fill_value=-1,
-            assume_sorted=True)
+                    bearings,
+                    range(len(bearings)),
+                    kind='linear',
+                    bounds_error=False,
+                    fill_value=-1,
+                    assume_sorted=True)
+        self.f_bearings = f_bearings
 
         #build the meshgrid
-        XX, YY = np.meshgrid(range(self.cols), range(self.rows))
-        x = self.res * (self.rows - YY)
-        y = self.res * (-self.cols / 2.0 + XX + 0.5)
-        b = np.arctan2(y, x) * self.REVERSE_Z
+        XX, YY = np.meshgrid(range(cols), range(rows))
+        x = range_resolution * (rows - YY)
+        y = range_resolution * (-cols / 2.0 + XX + 0.5)
+        b = np.arctan2(y, x) * -1
         r = np.sqrt(np.square(x) + np.square(y))
-        self.map_y = np.asarray(r / self.res, dtype=np.float32)
-        self.map_x = np.asarray(f_bearings(b), dtype=np.float32)
+        map_y = np.asarray(r / range_resolution, dtype=np.float32)
+        map_x = np.asarray(f_bearings(b), dtype=np.float32)
+
+        self.map_y = map_y
+        self.map_x = map_x
 
     def publish_features(self, ping, points):
         '''Publish the feature message using the provided parameters in an OculusPing message
@@ -211,13 +221,12 @@ class FeatureExtraction(object):
             img = np.frombuffer(sonar_msg.ping.data,np.uint8)
             img = np.array(cv2.imdecode(img,cv2.IMREAD_COLOR)).astype(np.uint8)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            
         #the image is not compressed, just use the ros numpy package
         else:
             img = ros_numpy.image.image_to_numpy(sonar_msg.ping)
 
         #generate a mesh grid mapping from polar to cartisian
-        self.generate_map_xy(sonar_msg)
+        self.generate_map(self.sonar_config)
 
         # Detect targets and check against threshold using CFAR (in polar coordinates)
         peaks = self.detector.detect(img, self.alg)
@@ -247,6 +256,6 @@ class FeatureExtraction(object):
             points = pcl.remove_outlier(
                 points, self.outlier_filter_radius, self.outlier_filter_min_points
             )
-
+        
         #publish the feature message
         self.publish_features(sonar_msg, points)
